e80b77f4bbf7b17b8b4177c9acafed6e8d61b175
cheesegameslive <cheesenatcho@gmail.com>
2020-08-02 04:01:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -268,7 +268,7 @@ function RandomString(length) {
   
 
        client.on('message', message=> {
-        if (message.channel.id !== '581795561854730252')
+        if (message.channel.id == '581795561854730252')
          client.channels.get('715493853733322783').send(message.content);
        });
 
